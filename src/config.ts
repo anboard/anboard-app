dabf64843dc78e2f2df8fc1b47c56a60360b40a5
cxdemxn <cxdemxn@gmail.com>
2024-11-25 16:59:29 +0100

    for vercel

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 const config = {
-    API_BASE_URL: 'http://localhost:1984/api/anb-broadcaster',
-    AUTH_BASE_URL: 'http://localhost:1984/auth',
-    API_ADMIN_URL: 'http://localhost:1984/api/admin'
-    //  API_BASE_URL: 'https://anboard-api.up.railway.app/api/anb-broadcaster',
-    // AUTH_BASE_URL: 'https://anboard-api.up.railway.app/auth',
-    // API_ADMIN_URL: 'https://anboard-api.up.railway.app/api/admin'
+    // API_BASE_URL: 'http://localhost:1984/api/anb-broadcaster',
+    // AUTH_BASE_URL: 'http://localhost:1984/auth',
+    // API_ADMIN_URL: 'http://localhost:1984/api/admin'
+    API_BASE_URL: 'https://anboard-api.up.railway.app/api/anb-broadcaster',
+    AUTH_BASE_URL: 'https://anboard-api.up.railway.app/auth',
+    API_ADMIN_URL: 'https://anboard-api.up.railway.app/api/admin'
 }
 
 export default config
