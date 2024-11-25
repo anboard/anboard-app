3785f94eb8b88c0a2afe7cbee1b64f041ec40320
cxdemxn <cxdemxn@gmail.com>
2024-11-25 17:13:05 +0100

    stubborn

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ const config = {
     // API_BASE_URL: 'http://localhost:1984/api/anb-broadcaster',
     // AUTH_BASE_URL: 'http://localhost:1984/auth',
     // API_ADMIN_URL: 'http://localhost:1984/api/admin'
-    API_BASE_URL: 'https://anboard-api.up.railway.app/api/anb-broadcaster',
+     API_BASE_URL: 'https://anboard-api.up.railway.app/api/anb-broadcaster',
     AUTH_BASE_URL: 'https://anboard-api.up.railway.app/auth',
     API_ADMIN_URL: 'https://anboard-api.up.railway.app/api/admin'
 }
