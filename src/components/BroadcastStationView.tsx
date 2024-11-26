998cf86f0342b5a89f1f1128002c66918ef89d2a
cxdemxn <cxdemxn@gmail.com>
2024-11-26 19:51:48 +0100

    fixed issue of station view not rendering when radio_shows array is null

--- src/components/BroadcastStationView.tsx
+++ src/components/BroadcastStationView.tsx
@@ -2,23 +2,20 @@ import React from "react";
 import IBroadcast from "../interface/IBroadcast";
 import styles from "../styles/broadcastview.module.css";
 // import Ivideo from "../interface/IVideo";
-import {  useOutletContext } from "react-router-dom";
-
+import { useOutletContext } from "react-router-dom";
 
 interface LayoutContext {
   //   userData: { name: string; email: string } | null;
   stationData: IBroadcast;
 }
 
-
 const BroadcastStationView: React.FC<LayoutContext> = () => {
   const { stationData }: LayoutContext = useOutletContext();
   // const { stationData }: LayoutContext = useOutletContext();
 
-    //  const navigate = useNavigate();
+  //  const navigate = useNavigate();
 
   return (
-    
     <div className={styles.container}>
       <h1>Broadcast Station</h1>
       <div className={styles.group}>
@@ -40,17 +37,14 @@ const BroadcastStationView: React.FC<LayoutContext> = () => {
       <div className={styles.group}>
         <h3>Radio Shows:</h3>
         <ol>
-          {stationData.radio_shows.map((show, index) => (
-            <li key={index}>{show}</li>
-          ))}
+          {stationData &&
+            stationData.radio_shows.map((show, index) => (
+              <li key={index}>{show}</li>
+            ))}
         </ol>
       </div>
     </div>
   );
 };
-
-
-
-
 
 export default BroadcastStationView;
