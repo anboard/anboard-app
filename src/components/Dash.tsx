a15ec05107ae5ae24ceac36700c97a2b7d0f8e66
cxdemxn <cxdemxn@gmail.com>
2024-12-08 06:10:30 +0100

    basic news and announcement sanity integration implemented

--- src/components/Dash.tsx
+++ src/components/Dash.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFileAudio,
@@ -16,53 +16,48 @@ import { useSwipeable, SwipeableHandlers } from "react-swipeable";
 import { useOutletContext } from "react-router-dom";
 // import { faBullhorn } from "@fortawesome/free-solid-svg-icons";
 
+import { FilteredResponseQueryOptions, type SanityDocument } from "@sanity/client";
+import { client } from "../sanity/client";
+
 interface LayoutContext {
   menuOpen: boolean;
   videoCount: string;
   audioCount: string;
-  lastVideoUpdateDate: string
-  lastAudioUpdateDate: string
+  lastVideoUpdateDate: string;
+  lastAudioUpdateDate: string;
 }
+
+interface sections {
+
+}
+
 const sections = [
   {
     title: "Admin Notifications",
-    items: [
-      {
-        title: "A titular title",
-        body: "Corresponding body for the titular title",
-        date: "Just now",
-      },
-      {
-        title: "Another title",
-        body: "Another corresponding body",
-        date: "Yesterday",
-      },
-    ],
+    items: [] as SanityDocument[],
   },
   {
     title: "ANBOARD News",
-    items: [
-      {
-        title: "Breaking News",
-        body: "Latest updates about ANBOARD activities",
-        date: "Today",
-      },
-      {
-        title: "Event Recap",
-        body: "Details about yesterday's ANBOARD event",
-        date: "Yesterday",
-      },
-    ],
+    items: [] as SanityDocument[]
   },
 ];
 
 const Dash: React.FC = () => {
   // const [isAdmin, setIsAdmin] = useState(true)
-  
-  const { menuOpen, audioCount, videoCount, lastVideoUpdateDate, lastAudioUpdateDate }: LayoutContext =
-    useOutletContext();
+
+  const {
+    menuOpen,
+    audioCount,
+    videoCount,
+    lastVideoUpdateDate,
+    lastAudioUpdateDate,
+  }: LayoutContext = useOutletContext();
 
   const [currentSectionIndex, setCurrentSectionIndex] = useState(0);
+  const [adminAnnouncements, setAdminAnnouncements] = useState<any[]>();
+  adminAnnouncements
+  const [anboardNews, setAnboardNews] = useState<any[]>();
+  anboardNews
 
   const handleSwipe = (direction: any) => {
     if (direction === "left") {
@@ -85,8 +80,54 @@ const Dash: React.FC = () => {
 
   const currentSection = sections[currentSectionIndex];
 
+  useEffect(() => {
+    const getAdminAnnouncement = async () => {
+      const POSTS_QUERY = `*[
+    _type == "admin_notifications"]|order(publishedAt desc)[0...12]{_id, title, body, publishedAt}`;
+
+      const options = { next: { revalidate: 30 } };
+
+      const posts = await client.fetch<SanityDocument[]>(
+        POSTS_QUERY,
+        {},
+        options as unknown as FilteredResponseQueryOptions
+      );
+
+      console.log(posts)
+      setAdminAnnouncements(posts)
+      sections[0] = { title: "Admin Notifications", items: posts }
+    };
+
+    getAdminAnnouncement();
+  }, []);
+
+  useEffect(() => {
+    const getAnboardNews = async () => {
+      const POSTS_QUERY = `*[
+    _type == "anboard_news"]|order(publishedAt desc)[0...12]{_id, title, body, publishedAt}`;
+
+      const options = { next: { revalidate: 30 } };
+
+      const posts = await client.fetch<SanityDocument[]>(
+        POSTS_QUERY,
+        {},
+        options as unknown as FilteredResponseQueryOptions
+      );
+
+      console.log(posts)
+      setAnboardNews(posts)
+      sections[1] = { title: 'ANBROAD News', items: posts }
+    };
+
+    getAnboardNews();
+  }, []);
+
   return (
-    <div className={`${dash.container} ${dash.small_grid}  ${menuOpen ? dash.single_grid : ""}`}>
+    <div
+      className={`${dash.container} ${dash.small_grid}  ${
+        menuOpen ? dash.single_grid : ""
+      }`}
+    >
       <div className={`${dash.audio_video_wrapper} ${dash.small_flex}`}>
         <div className={`${dash.pill}`}>
           <p className={`${dash.pill_title}`}>Vidoes</p>
@@ -99,7 +140,11 @@ const Dash: React.FC = () => {
             </div>
             <div className={`${dash.pill_info}`}>
               <p>{videoCount}</p>
-              <small>As of {lastVideoUpdateDate.split(' ')[1]} {lastVideoUpdateDate.split(' ')[2]}, {lastVideoUpdateDate.split(' ')[3]} </small>
+              <small>
+                As of {lastVideoUpdateDate.split(" ")[1]}{" "}
+                {lastVideoUpdateDate.split(" ")[2]},{" "}
+                {lastVideoUpdateDate.split(" ")[3]}{" "}
+              </small>
             </div>
           </div>
         </div>
@@ -115,13 +160,13 @@ const Dash: React.FC = () => {
             </div>
             <div className={`${dash.pill_info}`}>
               <p>{audioCount}</p>
-              <small>{
-                  lastAudioUpdateDate ? (
-                    `As of ${lastAudioUpdateDate.split(' ')[1]} ${lastAudioUpdateDate.split(' ')[2]}, ${lastAudioUpdateDate.split(' ')[3]}`
-                  )
-                  :
-                  '-'
-                }</small>
+              <small>
+                {lastAudioUpdateDate
+                  ? `As of ${lastAudioUpdateDate.split(" ")[1]} ${
+                      lastAudioUpdateDate.split(" ")[2]
+                    }, ${lastAudioUpdateDate.split(" ")[3]}`
+                  : "-"}
+              </small>
             </div>
           </div>
         </div>
@@ -150,8 +195,14 @@ const Dash: React.FC = () => {
               {currentSection.title}
             </div>
             <div className={dash.notif_title_control}>
-              <FontAwesomeIcon icon={faChevronLeft} onClick={() => handleSwipe('left')} />
-              <FontAwesomeIcon icon={faChevronRight} onClick={() => handleSwipe('right')} />
+              <FontAwesomeIcon
+                icon={faChevronLeft}
+                onClick={() => handleSwipe("left")}
+              />
+              <FontAwesomeIcon
+                icon={faChevronRight}
+                onClick={() => handleSwipe("right")}
+              />
             </div>
           </div>
           <div className={`${dash.notif_body}`}>
@@ -164,45 +215,11 @@ const Dash: React.FC = () => {
                 />
                 <div className="item_text_wrapper">
                   <div className={`${dash.item_title}`}>{item.title}</div>
-                  <div className="item_desc">{item.body}</div>
-                  <small className="item_date">{item.date}</small>
+                  <div className="item_desc">{item.body[0].children.map((child: any) => { return child.text})}</div>
+                  <small className="item_date">{item.publishedAt.split('T')[0]}</small>
                 </div>
               </div>
             ))}
-            {/* <div className={`${dash.notif_item}`}>
-            <img src="/images/logo.png" alt="" className={`${dash.notif_image}`} />
-            <div className="item_text_wrapper">
-              <div className={`${dash.item_title}`}>A titular title</div>
-              <div className="item_desc">
-                Corresponding body for the titular title
-              </div>
-              <small className="item_date">
-                Just now
-              </small>
-            </div>
-          </div>
-          <div className={`${dash.notif_item}`}>
-            <img src="/images/logo.png" alt="" className={`${dash.notif_image}`} />
-            <div className="item_text_wrapper">
-              <div className={`${dash.item_title}`}>A titular title</div>
-              <div className="item_desc">
-                Corresponding body for the titular title
-              </div>
-              <small className="item_date">Yesterday</small>
-            </div>
-          </div>
-          <div className={`${dash.notif_item}`}>
-            <img src="/images/logo.png" alt="" className={`${dash.notif_image}`} />
-            <div className="item_text_wrapper">
-              <div className={`${dash.item_title}`}>A titular title</div>
-              <div className="item_desc">
-                Corresponding body for the titular title
-              </div>
-              <small className="item_date">
-                10.11.24
-              </small>
-            </div>
-          </div> */}
           </div>
         </div>
       </div>
---
+++ src/sanity/client.ts
@@ -0,0 +1,9 @@
+// import { createClient } from "sanity";
+import { createClient } from '@sanity/client'
+
+export const client = createClient({
+  projectId: "5xv1zxx1",
+  dataset: "production",
+  apiVersion: "2024-01-01",
+  useCdn: false,
+});
