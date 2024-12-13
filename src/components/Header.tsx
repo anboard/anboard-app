fff32650db7a09a9420257d5843d181fc266d934
cxdemxn <cxdemxn@gmail.com>
2024-12-13 15:19:13 +0100

    progress is made

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,6 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { useAuth } from "../AuthContext";
 
-
 const Header: React.FC<{
   handleMenuClick: () => void;
   broadcaster: { upn: string; email: string };
@@ -44,8 +43,7 @@ const Header: React.FC<{
     setMenuOpen((prev) => !prev); // Toggle menu state
   };
 
-  useEffect(() => {
-  }, [menuOpen]);
+  useEffect(() => {}, [menuOpen]);
 
   const handleOutsideClick = (event: MouseEvent) => {
     event.stopPropagation();
@@ -72,90 +70,122 @@ const Header: React.FC<{
   }, [menuOpen]);
 
   useEffect(() => {
-    let newTitle = '';
-  if (location.pathname === "/api/anb-broadcaster/dashboard") {
-    setTitle("Dashboard");
-  } else if (location.pathname === "/api/anb-broadcaster/profile") {
-    setTitle("Profile");
-  } else if (location.pathname === "/api/anb-broadcaster/videos") {
-    setTitle("Videos");
-  } else if (location.pathname === "/api/anb-broadcaster/videos/upload") {
-    setTitle("Videos/upload");
-  } else if (location.pathname === "/api/anb-broadcaster/audios") {
-    setTitle("Audios");
-  } else if (location.pathname === "/api/anb-broadcaster/audios/upload") {
-    setTitle("Audios/upload");
-  } else if (location.pathname === "/api/anb-broadcaster/station") {
-    setTitle("Station");
-  }
-  setTitle(newTitle);
-}, [location.pathname, setTitle]);
+    let newTitle = "";
+    if (location.pathname === "/api/anb-broadcaster/dashboard") {
+      setTitle("Dashboard");
+    } else if (location.pathname === "/api/anb-broadcaster/profile") {
+      setTitle("Profile");
+    } else if (location.pathname === "/api/anb-broadcaster/videos") {
+      setTitle("Videos");
+    } else if (location.pathname === "/api/anb-broadcaster/videos/upload") {
+      setTitle("Videos/upload");
+    } else if (location.pathname === "/api/anb-broadcaster/audios") {
+      setTitle("Audios");
+    } else if (location.pathname === "/api/anb-broadcaster/audios/upload") {
+      setTitle("Audios/upload");
+    } else if (location.pathname === "/api/anb-broadcaster/station") {
+      setTitle("Station");
+    }
+    setTitle(newTitle);
+  }, [location.pathname, setTitle]);
 
   const handleImageLoad = () => {
     setIsLoaded(true);
   };
 
-  
   return (
-    <header className={`${header.container}  ${isMenuOpen ? header.shifted : ''}`}>
+    <header
+      className={`${header.container}  ${isMenuOpen ? header.shifted : ""}`}
+    >
       <div className={header.left}>
         {/* <div className={header.icon_wrapper} onClick={handleMenuClick}></div> */}
         <div>
-        <FontAwesomeIcon icon={faBars} className={`${header.icon_wrapper} ${layout.header} ${header.icon_big} ${isMenuOpen ? header.menu_hide : ''}`} onClick={handleMenuClick} />
-        <div className={`${header.icon_small}`} onClick={toggleMenu}>
-        <FontAwesomeIcon icon={faBars} className={`${header.icon_wrapper} ${layout.header}  ${isMenuOpen ? header.menu_hide : ''}`} />
-        </div>
-        {/* Dropdown Menu */}
-        {menuOpen && (
+          <FontAwesomeIcon
+            icon={faBars}
+            className={`${header.icon_wrapper} ${layout.header} ${
+              header.icon_big
+            } ${isMenuOpen ? header.menu_hide : ""}`}
+            onClick={handleMenuClick}
+          />
+          <div className={`${header.icon_small}`} onClick={toggleMenu}>
+            <FontAwesomeIcon
+              icon={faBars}
+              className={`${header.icon_wrapper} ${layout.header}  ${
+                isMenuOpen ? header.menu_hide : ""
+              }`}
+            />
+          </div>
+          {/* Dropdown Menu */}
+          {menuOpen && (
             <div className={header.dropdown} ref={dropdownRef}>
               <ul>
-                <li onClick={() => {
-                  navigate("/api/anb-broadcaster/dashboard")
-                  setMenuOpen(false)
-                }}>
+                <li
+                  onClick={() => {
+                    navigate("/api/anb-broadcaster/dashboard");
+                    setMenuOpen(false);
+                  }}
+                >
                   <FontAwesomeIcon
                     icon={faHouse}
                     className={header.notification}
                   />
                   Dashboard
                 </li>
-                <li onClick={() => {
-                  navigate("/api/anb-broadcaster/videos")
-                  setMenuOpen(false)
-                }}>
-            <FontAwesomeIcon icon={faFileVideo} className={header.notification} />
-
+                <li
+                  onClick={() => {
+                    navigate("/api/anb-broadcaster/videos");
+                    setMenuOpen(false);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faFileVideo}
+                    className={header.notification}
+                  />
                   Videos
                 </li>
-                <li onClick={() => {
-                  navigate("/api/anb-broadcaster/audios")
-                  setMenuOpen(false)
-                }}>
-        <FontAwesomeIcon icon={faFileAudio} className={header.notification} />
-
+                <li
+                  onClick={() => {
+                    navigate("/api/anb-broadcaster/audios");
+                    setMenuOpen(false);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faFileAudio}
+                    className={header.notification}
+                  />
                   Audios
                 </li>
-                <li onClick={() => {
-                  navigate("/api/anb-broadcaster/station")
-                  setMenuOpen(false)
-                }}>
-        <FontAwesomeIcon icon={faRadio} className={header.notification} />
-                  
+                <li
+                  onClick={() => {
+                    navigate("/api/anb-broadcaster/station");
+                    setMenuOpen(false);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faRadio}
+                    className={header.notification}
+                  />
                   Station
                 </li>
-                <li onClick={() => {
-                  navigate("/api/anb-broadcaster/profile")
-                  setMenuOpen(false)
-                }}>
-        <FontAwesomeIcon icon={faUser} className={header.notification} />
-
+                <li
+                  onClick={() => {
+                    navigate("/api/anb-broadcaster/profile");
+                    setMenuOpen(false);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    className={header.notification}
+                  />
                   Profile
                 </li>
                 <li onClick={logout}>
-        <FontAwesomeIcon icon={faDoorOpen} className={header.notification} />
-                  
+                  <FontAwesomeIcon
+                    icon={faDoorOpen}
+                    className={header.notification}
+                  />
                   Logout
-                  </li>
+                </li>
               </ul>
             </div>
           )}
@@ -168,7 +198,6 @@ const Header: React.FC<{
           <span className="directory-code">{broadcaster.upn}</span>
           <span className={header.directory_separator}></span>
           <span className={header.directory_location}>{title}</span>
-          
         </h1>
       </div>
 
@@ -176,7 +205,9 @@ const Header: React.FC<{
         <input type="search" name="header-search" />
       </div> */}
 
-      <div className={`${header.right}  ${isMenuOpen ? header.right_shifted : ''}`}>
+      <div
+        className={`${header.right}  ${isMenuOpen ? header.right_shifted : ""}`}
+      >
         <FontAwesomeIcon
           icon={faMagnifyingGlass}
           className={header.notification}
